de4ca04b9f1b0079c141db1466fa02f112d62753
alidhuniya <alihussaindhuniya0307@gmail.com>
2021-09-22 18:53:20 +0545

    rest-api

---
+++ README.md
@@ -0,0 +1,6 @@
+Prerequisites
+Have nodeJs installed on your machine
+Running the code
+clone the project by running git clone https://github.com/d-beloved/just-nodeJs-api-no-expressJs.git
+run npm install on your terminal (not exactly neccesary as no package was used in this project)
+run node server.js the starting point of the app
---
+++ userData.js
@@ -0,0 +1,20 @@
+module.exports = users = [
+    {
+      id: 1,
+      name: 'Seyi',
+      location: 'Buenos Aires',
+      description: 'Buenos Aires is one of the passionate developer and he always willing to learn new things so that he can be updated with tech markets.'
+    },
+    {
+      id: 2,
+      name: 'David Khan',
+      location: 'Argentina',
+      description: 'David Khan is one of the passionate developer and he always willing to learn new things so that he can be updated with tech markets.'
+    },
+    {
+      id: 3,
+      name: 'Isaac',
+      location: 'Kwala lumpor',
+      description: 'Buenos Aires is one of the passionate developer and he always willing to learn new things so that he can be updated with tech markets.'
+    }
+   ];
